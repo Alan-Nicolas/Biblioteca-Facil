9ffd3dea1f3449083f008427ae9917ab475f04d9
alann <alanicolax@gmail.com>
2025-05-11 16:05:32 -0400

    criando stage

--- src/main/java/com/example/gerenciador/ControlleBookControl.java
+++ src/main/java/com/example/gerenciador/ControlleBookControl.java
@@ -1,4 +1,11 @@
 package com.example.gerenciador;
 
+import javafx.stage.Stage;
+
 public class ControlleBookControl {
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 }
