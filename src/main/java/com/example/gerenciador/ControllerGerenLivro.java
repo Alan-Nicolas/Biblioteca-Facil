e5169de9fbb5a7865fb08b1724bec8b4b1fafaec
alann <alanicolax@gmail.com>
2025-05-17 11:07:20 -0400

    controller para os livros emprestados

---
+++ src/main/java/com/example/gerenciador/ControllerGerenLivro.java
@@ -0,0 +1,47 @@
+package com.example.gerenciador;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerGerenLivro {
+    @FXML
+    private TextField autorF;
+
+    @FXML
+    private TextField codF;
+
+    @FXML
+    private TextField titleF;
+
+    private Stage stage;
+
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+        stage.setTitle("Gerenciamento de Livro");
+    }
+
+    @FXML
+    private void voltarParaMenu () throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("menu.fxml"));
+        Parent root = loader.load();
+
+        MenuController controller = loader.getController();
+        controller.setStage(stage);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Menu");
+
+    }
+
+
+
+
+}
